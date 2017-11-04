83e290b2c0e8030927f2c043771bab82209e04c9
Tamara Nelson-Fromm <tln2@illinois.edu>
2017-11-04 13:58:45 -0500

    trying to parse

---
+++ src/Parent.java
@@ -0,0 +1,6 @@
+public class Parent {
+    int calendarYear;
+    int term;
+    String subject;
+    int course;
+}
---
+++ src/Section.java
@@ -0,0 +1,10 @@
+public class Section {
+    String sectionNumber;
+    String statusCode;
+    int partOfTerm;
+    String sectionStatusCode;
+    String enrollmentStatus;
+    String startDate;
+    String endDate;
+
+}
