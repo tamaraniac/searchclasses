d29f03001a376994e9a4111e08da5730163d31c4
Tamara Nelson-Fromm <tln2@illinois.edu>
2017-11-04 17:22:33 -0500

    more form stuff

--- WebApp/views.py
+++ WebApp/views.py
@@ -4,8 +4,10 @@ from WebApp import app
 
 @app.route('/')
 def index():
-    
 
-def specifications():
+@app.route('/coursesearch', methods=['GET', 'POST'])
+def CoursesForm():
     form = Specifications()
+    if form.validate_on_submit():
+        flash() #use this to display results
     return render_template('form.html' title = 'Course Search', form=form)
