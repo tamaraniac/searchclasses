77f55ba99afdb7e69d39193bf288094529421c71
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-11-04 16:56:51 -0500

    adding some routing

--- WebApp/views.py
+++ WebApp/views.py
@@ -1,5 +1,10 @@
 from flask import render_template, flash, redirect
 from .forms import Specifications
+from WebApp import app
+
+@app.route('/')
+def index():
+    
 
 def specifications():
     form = Specifications()
