0811fa0d99629f047e901453f3f304f7b51ebd30
Tamara Nelson-Fromm <tln2@illinois.edu>
2017-11-04 18:55:57 -0500

    variables from form

--- WebApp/views.py
+++ WebApp/views.py
@@ -10,5 +10,25 @@ from . import app
 def CoursesForm():
     form = Specifications()
     if form.validate_on_submit():
-        flash('no results')
+        term = form.term.data
+        year = form.year.data
+        department = form.department.data
+        coursenumber = form.coursenumber.data
+        gened1 = form.gened1.data
+        gened2 = form.gened2.data
+        gened3 = form.gened3.data
+        allorany = form.allorany.data
+        mon = form.mon.data
+        tue = form.tue.data
+        wed = form.wed.data
+        thur = form.thur.data
+        fri = form.fri.data
+        earliesttime = form.earliesttime.data
+        latesttime = form.latesttime.data
+        breakstart = form.breakstart.data
+        breakend = form.breakend.data
+        mainquad = form.mainquad.data
+        south = form.south.data
+        bardeen = form.bardeen.data
+        north = form.north.data
     return render_template('form.html', title='Course Search', form=form)
